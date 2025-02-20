a11ac018eba00b8926a69a5c30c273f352914bde
Ivan Barros <v-iBarros@lighthouseglobal.com>
2025-02-20 13:46:23 -0300

    refactor

---
+++ Hahn.Application/Controllers/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Hahn.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMemoryCache _cache;
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(IMediator mediator, IMemoryCache cache, ILogger<EventsController> logger)
+        {
+            _mediator = mediator;
+            _cache = cache;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Retrieves all Events.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "List of all Events.", typeof(IEnumerable<FoodRecipeDto>))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error.")]
+    }
+}
---
+++ Hahn.Data/Dtos/Events/EventsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.Data.Dtos.Events
+{
+    public class EventsDto
+    {
+    }
+}
